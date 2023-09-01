23afbd17d0068e4b6ff2007790e7524e99af1480
Dmitriy <dmitriy.shchebetovskyi@gmail.com>
2023-09-01 21:10:13 +0300

    commented code

--- main.py
+++ main.py
@@ -184,16 +184,18 @@
 # - вивести найменше із трьох чисел
 # - кол-во однакових чисел
 
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter second number: "))
-n3 = int(input("Enter third number: "))
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+#
+# # вывести наименьшее из трех чисел
+# if n1 < n2 < n3:
+#     print(n1)
+# elif n2 < n3 < n1:
+#     print(n2)
+# elif n3 < n2 < n1:
+#     print(n3)
+# else:
+#     print("All numbers equals")
 
-# вывести наименьшее из трех чисел
-if n1 < n2 < n3:
-    print(n1)
-elif n2 < n3 < n1:
-    print(n2)
-elif n3 < n2 < n1:
-    print(n3)
-else:
-    print("All numbers equals")
+
